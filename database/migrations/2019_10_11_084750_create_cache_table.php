c54d1f40871b9afc063b813b1f3e67a1fc204b72
Abdurrazaq <razaqofficial@gmail.com>
2019-10-11 09:51:36 +0100

    added session and cache table

--- database/migrations/2019_10_11_084750_create_cache_table.php
+++ database/migrations/2019_10_11_084750_create_cache_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLikesTable extends Migration
+class CreateCacheTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateLikesTable extends Migration
      */
     public function up()
     {
-        Schema::create('likes', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('project_id');
-            $table->integer('user_id');
-            $table->timestamps();
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->unique();
+            $table->mediumText('value');
+            $table->integer('expiration');
         });
     }
 
@@ -28,6 +27,6 @@ class CreateLikesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('likes');
+        Schema::dropIfExists('cache');
     }
 }
